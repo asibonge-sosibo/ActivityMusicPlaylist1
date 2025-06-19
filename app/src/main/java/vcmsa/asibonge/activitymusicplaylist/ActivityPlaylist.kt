4035a246c7926cec9bf2275335a0fc13bc525a2f
lab_services_student <asibongesosibo23@gmail.com>
2025-06-19 10:26:30 +0200

    Main Screen

--- app/src/main/java/vcmsa/asibonge/activitymusicplaylist/ActivityPlaylist.kt
+++ app/src/main/java/vcmsa/asibonge/activitymusicplaylist/ActivityPlaylist.kt
@@ -5,14 +5,12 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class ActivityPlaylist : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_playlist)
+        setContentView(R.layout.`activity_song_review.xml`)
 
         val displayList = findViewById<TextView>(R.id.displayList)
         val showTwoOrMore = findViewById<Button>(R.id.showTwoOrMore)
